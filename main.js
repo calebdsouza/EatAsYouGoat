c32348073c2095d5429218b52bfe30c74dd2d59e
lainiew <elaine.wu.ew@gmail.com>
2017-02-04 23:22:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function goat(username, originalPoints, tagList)
+{
+    this.name = username;
+    this.healthyPoints = orignalPoints; 
+    this.currnetLevel = "level/0.png";
+
+    
+    this.pointsRecalculator = function(foodTag, tagList){
+        tagList = tagList.sort();
+        tagListFindValue = tagList.find(foodTag);
+        //Check if the tag exists in the list 
+        if(typeof tagListFindValue == 'undefined')
+        {
+            this.healthyPoints -= 1;
+        }
+        else
+        {
+            this.healthyPoints += 1;
+        }//end if 
+    };
+    
+    this.getLevel = function(){
+        LEVELS = ['levels/0.png', 'levels/1.png', 'levels/2.png', 'levels/3.png', 'levels/4.png', 'levels/5.png', 'levels/6.png', 'levels/7.png', 'levels/8.png', 'levels/9.png', 'levels/10.png', 'levels/11.png', 'levels/12.png', 'levels/13.png', 'levels/14.png', 'levels/15.png', 'levels/16.png']
+        this.currentLevel = LEVEL[this.healthyPoints]; 
+    };
+    
+    
+    
+
+}
